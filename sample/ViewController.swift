b2724e7074a448e88b13862ae45fe4543a8a1d23
LiHS <lihsai0@gmail.com>
2020-01-15 15:45:58 +0800

    [UPDATE] learn UIView life cycle

--- sample/ViewController.swift
+++ sample/ViewController.swift
@@ -8,23 +8,46 @@
 
 import UIKit
 
+class TestView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        print("0. init")
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func willMove(toSuperview newSuperview: UIView?) {
+        super.willMove(toSuperview: newSuperview)
+        print("1. will move to new superview")
+    }
+    
+    override func didMoveToSuperview() {
+        super.didMoveToSuperview()
+        print("2. did move to new superview")
+    }
+    
+    override func willMove(toWindow newWindow: UIWindow?) {
+        super.willMove(toWindow: newWindow)
+        print("3. will move to new window")
+    }
+    
+    override func didMoveToWindow() {
+        super.didMoveToWindow()
+        print("4. did move to new window")
+    }
+}
+
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let view = UIView(frame: CGRect(
+        let view = TestView(frame: CGRect(
             x: 100, y: 100,
             width: 100, height: 100)
         )
         view.backgroundColor = .red
-        
-        let view2 = UIView(frame: CGRect(
-            x: 150, y: 150,
-            width: 100, height: 100)
-        )
-        view2.backgroundColor = .green
-        
         self.view.addSubview(view)
-        self.view.addSubview(view2)
     }
 }
