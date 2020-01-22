328a967f82ed3a5600175b837684b2f8525a4902
LiHS <lihsai0@gmail.com>
2020-01-22 15:40:30 +0800

    [UPDATE] use UITableViewDataSource

--- sample/Procedure/TableViewController.swift
+++ sample/Procedure/TableViewController.swift
@@ -8,13 +8,25 @@
 
 import UIKit
 
-class TableViewController: UIViewController {
+class TableViewController: UIViewController, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
         
         let tableView = UITableView(frame: self.view.bounds)
+        tableView.dataSource = self
 
         self.view.addSubview(tableView)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let tableViewCell = UITableViewCell(style: .subtitle, reuseIdentifier: "id")
+        tableViewCell.textLabel?.text = "主标题"
+        tableViewCell.detailTextLabel?.text = "副标题"
+        return tableViewCell
     }
 }
