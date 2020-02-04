f116b294897041042258cf6aa4118c0d484a0bd9
LiHS <lihsai0@gmail.com>
2020-02-04 22:21:03 +0800

    [UPDATE] custom UITableViewCell

---
+++ sample/Procedure/MyTableCell.swift
@@ -0,0 +1,80 @@
+//
+//  MyCell.swift
+//  sample
+//
+//  Created by lihs on 2020/2/4.
+//  Copyright © 2020 lihs. All rights reserved.
+//
+
+import UIKit
+
+class MyTableCell: UITableViewCell {
+    
+    private lazy var titleLable: UILabel = {
+        let l = UILabel(frame: CGRect(x: 20, y: 15, width: 300, height: 50))
+        l.font = UIFont.systemFont(ofSize: 16)
+        return l
+    }()
+    private lazy var sourceLable: UILabel = {
+        let l = UILabel(frame: CGRect(x: 20, y: 80, width: 50, height: 20))
+        l.font = UIFont.systemFont(ofSize: 12)
+        return l
+    }()
+    private lazy var commentNumLable: UILabel = {
+        let l = UILabel(frame: CGRect(x: 85, y: 80, width: 50, height: 20))
+        l.font = UIFont.systemFont(ofSize: 12)
+        return l
+    }()
+    private lazy var timeLable: UILabel = {
+        let l = UILabel(frame: CGRect(x: 150, y: 80, width: 50, height: 20))
+        l.font = UIFont.systemFont(ofSize: 12)
+        return l
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.contentView.addSubview(titleLable)
+        self.contentView.addSubview(sourceLable)
+        self.contentView.addSubview(commentNumLable)
+        self.contentView.addSubview(timeLable)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func layoutTableViewCell() {
+        titleLable.text = "学习 iOS 开发"
+        
+        sourceLable.text = "lihs"
+        sourceLable.sizeToFit()
+        
+        commentNumLable.text = "1 评论"
+        commentNumLable.sizeToFit()
+        commentNumLable.frame = CGRect(
+            x: sourceLable.frame.origin.x + sourceLable.frame.width + 15,
+            y: commentNumLable.frame.origin.y,
+            width: commentNumLable.frame.size.width,
+            height: commentNumLable.frame.size.height)
+        
+        timeLable.text = "刚刚"
+        timeLable.sizeToFit()
+        timeLable.frame = CGRect(
+            x: commentNumLable.frame.origin.x + commentNumLable.frame.width + 15,
+            y: timeLable.frame.origin.y,
+            width: timeLable.frame.width,
+            height: timeLable.frame.height)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- sample/Procedure/ViewController.swift
+++ sample/Procedure/ViewController.swift
@@ -8,91 +8,39 @@
 
 import UIKit
 
-// MARK: TestView
-class TestView: UIView {
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        print("view: 0. init")
-    }
-    
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-    }
-    
-    override func willMove(toSuperview newSuperview: UIView?) {
-        super.willMove(toSuperview: newSuperview)
-        print("view: 1. will move to new superview")
-    }
-    
-    override func didMoveToSuperview() {
-        super.didMoveToSuperview()
-        print("view: 2. did move to new superview")
-    }
-    
-    override func willMove(toWindow newWindow: UIWindow?) {
-        super.willMove(toWindow: newWindow)
-        print("view: 3. will move to new window")
-    }
-    
-    override func didMoveToWindow() {
-        super.didMoveToWindow()
-        print("view: 4. did move to new window")
-    }
-}
-
-// MARK: ViewController
-class ViewController: UIViewController {
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-        print("view controller: 0. init")
-    }
-    
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        print("view controller: 1. default view will appear")
-    }
-
-    override func viewDidAppear(_ animated: Bool){
-        super.viewDidAppear(animated)
-        print("view controller: 2. default view did appear")
-    }
-
-    override func viewWillDisappear(_ animated: Bool){
-        super.viewWillDisappear(animated)
-        print("view controller: 3. default view will disappear")
-    }
-
-    override func viewDidDisappear(_ animated: Bool){
-        super.viewDidDisappear(animated)
-        print("view controller: 4. default view did disappear")
-    }
-    
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let view = TestView(frame: CGRect(
-            x: 100, y: 100,
-            width: 100, height: 100)
-        )
-        view.backgroundColor = .red
-        self.view.addSubview(view)
+        let tableView = UITableView(frame: self.view.bounds)
+        tableView.dataSource = self
+        tableView.delegate = self
         
-        let tapGesture = UITapGestureRecognizer(
-            target: self, action: #selector(pushController))
-        view.addGestureRecognizer(tapGesture)
+        self.view.addSubview(tableView)
     }
     
-    @objc func pushController() {
-        let viewController = UIViewController()
-        viewController.view.backgroundColor = .white
-        viewController.navigationItem.title = "内容"
-        viewController.navigationItem.rightBarButtonItem = UIBarButtonItem(
-            title: "右侧标题", style: .plain, target: self, action: nil)
+    // MARK: UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "id")
         
-        self.navigationController?.pushViewController(viewController, animated: true)
+        if cell == nil {
+            cell = MyTableCell(style: .subtitle, reuseIdentifier: "id")
+        }
+
+        // 确保安全性，Object-C 似乎是隐式转换，也可能有风险
+        if let myCell = cell as? MyTableCell {
+            myCell.layoutTableViewCell()
+        }
+
+        return cell!
+    }
+    
+    // MARK: UITableViewDelegate
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 115
     }
 }
