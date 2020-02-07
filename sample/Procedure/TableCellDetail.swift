f47a08eba437deed9d73e75eef13837dadb3cfa6
LiHS <lihsai0@gmail.com>
2020-02-07 16:06:00 +0800

    [UPDATE] add progressView using KVO

--- sample/Procedure/TableCellDetail.swift
+++ sample/Procedure/TableCellDetail.swift
@@ -10,22 +10,46 @@ import UIKit
 import WebKit
 
 class TableCellDetail: UIViewController, WKNavigationDelegate {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        let webView = WKWebView(frame: CGRect(
+    private lazy var webView: WKWebView = {
+        let wv = WKWebView(frame: CGRect(
             x: 0,
             y: 0,
             width: self.view.bounds.width,
             height: self.view.bounds.height))
-        webView.navigationDelegate = self
+        wv.navigationDelegate = self
+        
+        return wv
+    }()
+    
+    private lazy var progressView: UIProgressView = {
+        let p = UIProgressView(frame: CGRect(
+            x: 0,
+            y: 88,
+            width: self.view.bounds.width,
+            height: 20))
+        return p
+    }()
+    
+    private var observation: NSKeyValueObservation?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        observation = webView.observe(\.estimatedProgress, options: .new, changeHandler: {wv, _ in
+            let progress = wv.estimatedProgress
+            self.progressView.setProgress(Float(progress), animated: true)
+        })
         
         self.view.addSubview(webView)
+        self.view.addSubview(progressView)
         
         if let url = URL(string: "https://lihs.me/") {
             webView.load(URLRequest(url: url))
         }
+    }
+    
+    deinit {
+        observation?.invalidate()
     }
     
     // MARK: WKNavigationDelegate
